f932db7aad4cc262d0668303bfbfcddd2f884ead
Pavukoplov <arachnoplov@gmail.com>
2024-03-14 00:59:34 +0300

    replace Timer to ScheduledExecutorService

--- src/main/java/lab/andersen/listener/AppContextListener.java
+++ src/main/java/lab/andersen/listener/AppContextListener.java
@@ -1,32 +1,26 @@
 package lab.andersen.listener;
 
+import lab.andersen.util.TaskExecutor;
 import lab.andersen.util.TelegramSender;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import java.util.Timer;
-import java.util.TimerTask;
 
 @WebListener
 public class AppContextListener implements ServletContextListener {
-    private Timer timer;
+    private TaskExecutor taskExecutor;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        timer = new Timer(true);
-        //timer.schedule(new SendPdfJob(), 0, 10000);
+        taskExecutor = new TaskExecutor(() -> {
+            TelegramSender.sendMessage("test daily job");
+        });
+        taskExecutor.startExecutionAt(23, 55, 0);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        timer.cancel();
-    }
-
-    private static class SendPdfJob extends TimerTask {
-        @Override
-        public void run() {
-            TelegramSender.sendMessage("timer test");
-        }
+        taskExecutor.stop();
     }
 }
