86363d6e085a1415eda9a42c410437dc72b73e00
Pavukoplov <arachnoplov@gmail.com>
2024-03-18 18:50:43 +0300

    add tests

--- src/test/java/lab/andersen/UserServiceUnitTest.java
+++ src/test/java/lab/andersen/UserServiceUnitTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class UserServiceUnitTest {
@@ -64,9 +65,26 @@ public class UserServiceUnitTest {
     public void findUserById_throwsException() throws DaoException {
         when(userDao.findById(anyInt())).thenReturn(Optional.empty());
 
-        Assertions.assertThrows(UserNotFoundException.class,
+        assertThrows(UserNotFoundException.class,
                 () -> userService.findById(anyInt())
         );
     }
 
+    @Test
+    public void createUser() throws DaoException, ServiceException {
+        User user = new User();
+        doNothing().when(userDao).create(user);
+
+        userService.create(user);
+
+        verify(userDao, times(1)).create(user);
+    }
+
+    @Test
+    public void createUser_DaoException() throws DaoException {
+        User user = new User();
+        doThrow(DaoException.class).when(userDao).create(user);
+
+        assertThrows(ServiceException.class, () -> userService.create(user));
+    }
 }
