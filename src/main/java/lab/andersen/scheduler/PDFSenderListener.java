1307b12ef2c50e176b3573bc38e76b7b22838da3
nurbq <nurbakhyt.sembayev@gmail.com>
2024-03-18 11:50:42 +0500

    change time

--- src/main/java/lab/andersen/scheduler/PDFSenderListener.java
+++ src/main/java/lab/andersen/scheduler/PDFSenderListener.java
@@ -23,8 +23,8 @@ public class PDFSenderListener implements ServletContextListener {
     private static final String BODY_MESSAGE = "Attaching files";
     private static final String SUBJECT_MESSAGE = "Activity of users";
     private static final String FULL_PATH_PDF = PropertiesUtils.get("pdf.base.url") + LocalDate.now() + ".pdf";
-    private static final int TARGET_HOUR = 10;
-    private static final int TARGET_MIN = 15;
+    private static final int TARGET_HOUR = 11;
+    private static final int TARGET_MIN = 55;
     private static final int TARGET_SEC = 0;
 
     @Override
